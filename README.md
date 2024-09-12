eb9761a612af5da3bf67fd50b3de595875800c40
lswitch18 <wellyntonjeronimo@outlook.com>
2024-09-12 12:50:06 -0300

    FIX: subnet correction

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Este é um projeto pessoal de Criação de uma infraestrutura de TI completa na AWS que ira rodar serviços conteinerizados e orquestrados pelo Kubernetes.
 basicamete no código consta a utilização dos AWS web services abaixo.
 
-PROVIDER = AWS
+
 VPC
 EC2
 RDS
-ECK KUBERNETES 
+EKS KUBERNETES 
 S3
 Secret Manager
--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      
+
     }
   }
 
   backend "s3" {
     bucket = "statebackendbr20241990uasdi"
-    key    = "PROD/terraform.tfstate"  # Ajuste para um caminho mais específico
+    key    = "PROD/terraform.tfstate" # Ajuste para um caminho mais específico
     region = "us-east-1"
   }
 }
@@ -19,8 +19,8 @@ provider "aws" {
 
 # VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "Enterprise-vpc"
@@ -29,9 +29,9 @@ resource "aws_vpc" "my_vpc" {
 
 # Subnet Pública
 resource "aws_subnet" "public_subnet" {
-  vpc_id                 = aws_vpc.my_vpc.id
-  cidr_block             = "10.0.1.0/24"
-  availability_zone      = "us-east-1a"  # Zona de disponibilidade específica
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Zona de disponibilidade específica
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
@@ -39,12 +39,34 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # Subnet Privada
+resource "aws_subnet" "public_subnet2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a" # Ajuste conforme a zona de disponibilidade desejada
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet2"
+  }
+}
+
+# Subnet Privada 1
 resource "aws_subnet" "private_subnet" {
-  vpc_id                 = aws_vpc.my_vpc.id
-  cidr_block             = "10.0.2.0/24"
-  availability_zone      = "us-east-1a"  # Ajuste conforme a zona de disponibilidade desejada
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b" # Zona de disponibilidade específica
+  map_public_ip_on_launch = true
   tags = {
     Name = "private-subnet"
+  }
+}
+
+# Subnet Privada 2
+resource "aws_subnet" "private_subnet2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b" # Ajuste conforme a zona de disponibilidade desejada
+  tags = {
+    Name = "private_subnet2"
   }
 }
 
@@ -87,3 +109,4 @@ resource "aws_route_table_association" "private_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
